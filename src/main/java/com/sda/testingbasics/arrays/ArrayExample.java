a0bdc7cc1969b632981c49c81b155ce08dd05aac
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-11-20 15:31:59 +0100

    ArrayExample failing test

---
+++ src/main/java/com/sda/testingbasics/arrays/ArrayExample.java
@@ -0,0 +1,8 @@
+package com.sda.testingbasics.arrays;
+
+public class ArrayExample {
+
+	public static String[] removeDuplicates(String[] array) {
+		return new String[0];
+	}
+}
---
+++ src/test/java/com/sda/testingbasics/arrays/ArrayExampleTest.java
@@ -0,0 +1,25 @@
+package com.sda.testingbasics.arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ArrayExampleTest {
+
+	@Test
+	void shouldReturnArrayWithDistinctElements() {
+		//given
+		//create array ["a", "b", "a", "c"]
+		String[] withDuplicates = {"a", "b", "a", "c"};
+
+		//when
+		String[] distinctArray = ArrayExample.removeDuplicates(withDuplicates);
+
+		//then
+		//distinctArray = ["a", "b", "c"]
+		//assertEquals(distinctArray, new String[]{"a", "b", "c"});
+		assertThat(distinctArray)
+				.containsExactlyInAnyOrder("a", "b", "c");
+	}
+}
