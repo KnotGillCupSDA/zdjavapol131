9ab396f23fd9dfe492d1b6048bcd32144df8d5f1
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-11-20 11:22:32 +0100

    AirplaneTest first test

---
+++ src/test/java/com/sda/testingbasics/air/AirplaneTest.java
@@ -0,0 +1,20 @@
+package com.sda.testingbasics.air;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AirplaneTest {
+
+	@Test
+	void shouldAscent() {
+		//given
+		//create Airplane
+
+		//when
+		//call ascent method
+
+		//then
+		//assert that getHeight returns expected value
+	}
+}
