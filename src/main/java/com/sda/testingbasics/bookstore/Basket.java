4e684cb8d69ef371005939d6b08fe5794e2550b2
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-11-20 14:18:45 +0100

    BasketTest mid work

--- src/main/java/com/sda/testingbasics/bookstore/Basket.java
+++ src/main/java/com/sda/testingbasics/bookstore/Basket.java
@@ -1,12 +1,21 @@
 package com.sda.testingbasics.bookstore;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Basket {
+
+	private final List<Book> books;
+
+	public Basket() {
+		books = new ArrayList<>();
+	}
 
 	public int size() {
 		return 0;
 	}
 
 	public void add(Book book) {
-
+		books.add(book);
 	}
 }
