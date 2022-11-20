1dd4f83df540ac02ac60d4b30e6dfbbf69b885d0
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-11-20 10:57:14 +0100

    test adding two positive numbers

--- src/test/java/com/sda/testingbasics/calculator/CalculatorTest.java
+++ src/test/java/com/sda/testingbasics/calculator/CalculatorTest.java
@@ -8,6 +8,16 @@ class CalculatorTest {
 
 	@Test
 	void shouldAddTwoPositiveNumbers() {
+		//given
+		double a = 1.0;
+		double b = 2.0;
 
+		//when
+		double sum = new Calculator().add(a, b);
+
+		//then
+		//if(sum == 3) -> ok (green)
+		//else -> fail (red)
+		assertEquals(3.0, sum, "1 + 2 should equals 3");
 	}
 }
